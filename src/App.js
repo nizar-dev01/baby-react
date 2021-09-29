0524b57cc32c702c7ac048c5a189e92207b9a826
nizar <nizar.dev01@gmail.com>
2021-09-29 13:09:54 +0530

    Implemented react-router-dom

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import { header as Header } from "@c/header";
 import { Tasks } from '@c/tasks'
 import { useState } from 'react';
+import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom'
 function App() {
   const [tasks, setTasks] = useState([
     {
@@ -13,11 +14,34 @@ function App() {
       title: 'Second Task'
     },
   ])
+  const deleteTask = (task) => {
+    setTasks(tasks.filter(t => t.id !== task.id))
+  }
   return (
-    <main style={{ width: '90%', maxWidth: '600px', margin: '0 auto'}}>
-      <Header title={1} />
-      <Tasks tasks={tasks}></Tasks>
-    </main>
+    <Router>
+      <header>this is the header</header>
+      <Link to="/test">Test link</Link>
+
+      <Switch>
+        <Route exact path="/" render={(props) => (
+          <main style={{ width: '90%', maxWidth: '600px', margin: '0 auto' }}>
+            <Header title={1} />
+            <Tasks tasks={tasks} onDelete={deleteTask}></Tasks>
+          </main>
+        )} />
+        <Route render={(props) => (
+          <>
+            <h1>404</h1>
+            <Link to="/">Back Home</Link>
+          </>
+        )} />
+      </Switch>
+
+
+      <footer>
+        this is the footer
+      </footer>
+    </Router>
   );
 }
 
--- src/components/tasks.js
+++ src/components/tasks.js
@@ -1,17 +1,19 @@
 import { IndivTask } from './indivTask'
-export const Tasks = ({ tasks }) => {
+import { useState } from 'react'
+import InputMask from 'react-input-mask';
+
+export const Tasks = ({ tasks, onDelete }) => {
+    const [name, setName] = useState("");
+    const bindName = e => setName(e.target.value)
     return (
         <div style={{ padding: '20px' }}>
             {
                 tasks.map((t, i) => (
-                    <IndivTask task={t} key={i} onDelete={deleteTask} />
+                    <IndivTask task={t} key={i} onDelete={onDelete} />
                 ))
             }
+            <InputMask mask='(+1) 999 999 9999' value={name} onInput={bindName} />
+            <InputMask mask='(+1) 999 999 9999' value={name} onInput={bindName} />
         </div>
     )
-}
-
-
-const deleteTask = (task) => {
-    console.log(task)
 }
