68e0b57de0abd70b37c8c5fd16c6c217a2c59c0e
nizar <nizar.dev01@gmail.com>
2021-10-04 15:41:55 +0530

    Implemented useEffect hook in Header component

--- src/components/header.js
+++ src/components/header.js
@@ -1,12 +1,14 @@
 import { Button } from '@c/button'
-import { useReducer } from 'react'
+import { useReducer, useEffect } from 'react'
 
 const reducer = (state, action) => {
     switch (action) {
         case 'increment':
-            return { count: state.count + 1 }
+            return { count: state.count + 1, bool: state.bool }
         case 'decrement':
-            return { count: state.count - 1 }
+            return { count: state.count - 1, bool: state.bool }
+        case 'bool':
+            return { count: state.count, bool: !state.bool}
         default:
             return state
         
@@ -14,18 +16,21 @@ const reducer = (state, action) => {
 }
 
 export const Header = () => {
-    const [state, dispatch] = useReducer(reducer, { count: 1 })
+    const [state, dispatch] = useReducer(reducer, { count: 1, bool: false })
+    // Just trying the useEffect hook
+    useEffect(() => {
+        console.count('calling useEffect');
+    },[state.bool])
     return (
         <header style={{ padding: '20px', display: 'flex', justifyContent: 'space-between' }}>
 
             <h1>Task Tracker : {state.count}</h1>
             <Button onClick={() => {
-                console.log('asdf')
                 dispatch('increment')
             }}>Increase</Button>
             <Button onClick={() => {
-                console.log('a;lakl')
                 dispatch('decrement')
+                dispatch('bool')
             }}>Decrease</Button>
         </header>
     )
